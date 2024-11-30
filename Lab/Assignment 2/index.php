8b4f910eb91ed4e4ae3ffa3b9c3e81475075718b
UndergradAlgoExpert <ali.collage.student@gmail.com>
2024-12-01 00:40:09 +0300

    Used different link to picoCSS (One in the docomentation);

--- Lab/Assignment 2/index.php
+++ Lab/Assignment 2/index.php
@@ -18,7 +18,7 @@ Student IDs: 202105103, 202208581
     <title>Student Enrollment Information</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <link rel="stylesheet" href="styles.css">
   </head>
   <body>
