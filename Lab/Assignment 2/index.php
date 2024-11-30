c78aeea7d935c1bfc94b49973173b250c8b79762
Ali Mohamed Ali Hassan <100236390+another4433@users.noreply.github.com>
2024-11-30 23:01:03 +0300

    Update on Student Enrollment Statistics
    This file contains code of retrieving data from API and displaying it in form of table.

--- Lab/Assignment 2/index.php
+++ Lab/Assignment 2/index.php
@@ -3,7 +3,7 @@
 //API call
 $URL = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
 
-$reponse = file_get_contents($URL);
+$reponse = file_get_contents($URL); //Reading content from API
 $result = json_decode($reponse); //API results
 ?>
 
@@ -16,17 +16,17 @@ Student IDs: 202105103
 <html lang="en">
   <head>
     <title>Student Enrollment Statistics</title>
-    <meta name="Data Encoder" charset="utf-8">
-    <meta name="Color Scheme" content="light dark">
-    <meta name="Website Layout" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <meta name="Data Encoder" charset="utf-8"> <!--Format used to read data in the website-->
+    <meta name="Color Scheme" content="light dark"> <!--Applying theme to the webpage to be in the color between white and dark-->
+    <meta name="Website Layout" content="width=device-width, initial-scale=1.0"> <!--Allowing the page to be responsive on any device-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"> <!--CSS Framework used to apply designs and styles to the website-->
+    <link rel="stylesheet" href="styles.css"> <!--Custom CSS style that helps applying more style to the website-->
   </head>
   <body>
     <header>UOB IT Collage Enrollement Data (2018-2023)</header>
     <main class="pico">
       <table>
-        <thead data-theme="light">
+        <thead data-theme="light"> <!--It is used to group the headers together to apply certain style to it-->
           <tr>
             <th>Year</th>
             <th>Semester</th>
@@ -36,8 +36,8 @@ Student IDs: 202105103
             <th>Number of Students</th>
           </tr>
         </thead>
-        <tbody>
-          <?php foreach($result->results as $semester): ?>
+        <tbody> <!--It is another group in tables that shows the contents retrieved from the website-->
+          <?php foreach($result->results as $semester): ?> <!--It inserts data retrieved from API into an array and identify each row as 'semester'-->
             <tr>
               <td><?=$semester->year?></td>
               <td><?=$semester->semester?></td>
@@ -45,8 +45,8 @@ Student IDs: 202105103
               <td><?=$semester->nationality?></td>
               <td><?=$semester->colleges?></td>
               <td><?=$semester->number_of_students?></td>
-            </tr>
-          <?php endforeach?>
+            </tr> <!--Each row includes table format like this to be adjusted with the table headers-->
+          <?php endforeach?> <!--Each row contains the column as year, semester, program, nationality, college, and number of students-->
         </tbody>
       </table>
     </main>
